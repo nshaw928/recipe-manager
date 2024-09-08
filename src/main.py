2f210d2e08e4d8a096e2de04f021862371ceb917
nshaw928 <nshaw928@gmail.com>
2024-09-08 00:27:33 -0400

    added sidebar

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from recipeClass import Recipe
-from PySide6.QtWidgets import QApplication, QWidget, QMainWindow, QVBoxLayout, QHBoxLayout, QTextEdit
+from PySide6.QtWidgets import QApplication, QWidget, QMainWindow, QVBoxLayout, QHBoxLayout, QTextEdit, QListWidget
 import sys
+import os
 
 class AppWindow(QMainWindow):
     def __init__(self):
@@ -14,20 +15,29 @@ class AppWindow(QMainWindow):
                 }
         ''')
 
-
         self.main_window = QWidget()
         self.layout = QHBoxLayout(self.main_window)
         self.setCentralWidget(self.main_window)
 
         self.init_ui()
+        self.load_sidebar_items('/home/nshaw928/Documents/Obsidian Vault/Recipes')
+        self.markdown_update('/home/nshaw928/Documents/Obsidian Vault/Recipes/German Curry Sauce.md')
 
     def init_ui(self):
+        self.sidebar = QListWidget()
         self.md_viewer = QTextEdit(readOnly=True)
         
+        self.layout.addWidget(self.sidebar)
         self.layout.addWidget(self.md_viewer)
 
+    def load_sidebar_items(self, recipe_folder_path):
+        files = os.listdir(recipe_folder_path)
+        print(files)
+        for file_name in files:
+            self.sidebar.addItem(file_name)
+
     def markdown_update(self, path):
-        md_text = 0 # Read path of md file
+        md_text = open(path, 'r').read()
         self.md_viewer.setMarkdown(md_text)
 
 if __name__ == "__main__":
